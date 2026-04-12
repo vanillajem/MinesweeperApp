a15acb8163b76a1d0012645d814e03526cde019d
Jacob Cutler <cutlerjacob18@gmail.com>
2026-04-12 11:37:13 -0700

    Add files via upload
    Add migrations for the game score table

---
+++ Migrations/20260412181720_AddGameScoresTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinesweeperApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameScoresTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "GameScores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "GameScores");
+        }
+    }
+}
